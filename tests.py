124e8ea6e76ea757432ac9afbe54dd681fb995f3
Olga <olga.strizhko@akvelon.com>
2019-02-19 15:20:58 +0300

    sqlite writer

--- tests.py
+++ tests.py
@@ -6,38 +6,13 @@ import unittest
 from readers import CSVReader
 from writers import CSVWriter
 
-CONFIG_PATH = 'settings.ini'
-
-
-def createConfig(path):
-    """
-    Create a config file
-    """
-    config = configparser.ConfigParser()
-    config['Settings'] = {}
-    config['Settings']['file_name'] = "./test_csv.csv"
-    config['Settings']['write_file_name'] = "./write_test_csv.csv"
-    # config.add_section('fmtparams')
-    # config['fmtparams']['encoding']='utf8'
-
-    # config.add_section('Settings')
-    # config.set('Settings', "file_name", "./test_csv.csv")
-    # config.set('Settings', "write_file_name", "./write_test_csv.csv")
-    # config.add_section('fmtparams')
-
-    with open(path, "w") as config_file:
-        config.write(config_file)
-
 
 class ReaderTestCase(unittest.TestCase):
     """ Тесты reader-объекта """
     def test_csv_reader(self):
         # test_file_name = './test_csv.csv'
-        # create config
-        createConfig(CONFIG_PATH)
 
-        config = configparser.ConfigParser()
-        config.read(CONFIG_PATH)
+        config = {'type':'csv','file_name':'./test_csv.csv'}
         reader = CSVReader(config)
         data = list(reader)
         print('reader test')
@@ -50,17 +25,15 @@ class WriterTestCase(unittest.TestCase):
     """ Тесты writer-объекта """
 
     def test_csv_writer(self):
-        config = configparser.ConfigParser()
-        config.read(CONFIG_PATH)
+        config = {'type': 'csv', 'file_name': './test_csv.csv'}
 
         reader = CSVReader(config)
         rows = list(reader)
         print('\n rows:')
         print(rows)
-        writer = CSVWriter(config)
+        writer = CSVWriter(config={'type': 'csv', 'file_name': './write_test_csv.csv'})
         writer.write(rows)
-        config.set('Settings', "file_name", "./write_test_csv.csv")
-        reader = CSVReader(config)
+        reader = CSVReader(config={'type': 'csv', 'file_name': './write_test_csv.csv'})
         data = list(reader.read())
         print('writer test')
         self.assertEqual(len(data), 7)
@@ -74,5 +47,4 @@ class PipelineTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    createConfig(CONFIG_PATH)
     unittest.main()
